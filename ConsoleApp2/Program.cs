20c4279567193ea0fb1772447908f95e43033dc6
msi <msi@DESKTOP-2D07ETA>
2023-11-29 08:31:44 -0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              Access Modified:
+              
+              public
+              private
+              protect
+              internal
+              protected internal
+             */
+
+        }
+
+        //Methodun Signature(imzasi)
+        //Access modified + Geri donus tipi + Metodun adi (input deyerler
+        public void GetArray()
+        {
+            string choose = "";
+            while(choose != "1")
+            {
+                string[] arr;
+                Console.WriteLine("Arrayin uzunlugunu daxil edin");
+                int num = Convert.ToInt32(Console.ReadLine());
+                arr = new string[num];
+                while (choose != "2");
+                {
+                    choose = Console.ReadLine();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
